859195318b7658485ac121668eaef19562e884f7
Kamil Khismatullin <kami16ru@gmail.com>
2020-10-06 11:55:24 +0300

    B2TICKETS-164 frontend env variables set

--- src/plugins/echo.js
+++ src/plugins/echo.js
@@ -4,16 +4,17 @@ import axios from '@/plugins/axios'
 
 export default new Echo({
   broadcaster: 'pusher',
-  key: 'websocketkey',
+  key: process.env.VUE_APP_WS_KEY,
   forceTLS: false,
-  wsHost: '127.0.0.1',
-  wsPort: 6001,
+  wsHost: process.env.VUE_APP_WS_HOST,
+  wsPort: process.env.VUE_APP_WS_PORT,
+  wssPort: process.env.VUE_APP_WS_PORT,
   disableStats: true,
   encrypted: true,
   authorizer: (channel, options) => {
     return {
       authorize: (socketId, callback) => {
-        axios.post('http://localhost/broadcasting/auth', {
+        axios.post(process.env.VUE_APP_API_BASE_URL + '/broadcasting/auth', {
           socket_id: socketId,
           channel_name: channel.name
         })
