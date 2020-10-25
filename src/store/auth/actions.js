71ed74872ccefab20baf99f41ac05922dd0dd3bf
Kamil Khismatullin <kami16ru@gmail.com>
2020-10-26 02:11:18 +0300

    chat user auth

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -3,14 +3,14 @@ import axios from '@/plugins/axios'
 const signIn = async ({ dispatch }, credentials) => {
   await axios.get(process.env.VUE_APP_API_BASE_URL + '/sanctum/csrf-cookie')
     .then( async () => {
-      await axios.post(process.env.VUE_APP_API_BASE_URL + '/login', credentials).then(async() => {
+      await axios.post(process.env.VUE_APP_API_BASE_URL + route('login'), credentials).then(async() => {
         await dispatch('main')
       })
     })
 }
 
 const signOut = async ({ dispatch }) => {
-  await axios.post(process.env.VUE_APP_API_BASE_URL + '/logout').then(async() => {
+  await axios.post(process.env.VUE_APP_API_BASE_URL + route('logout')).then(async() => {
     await dispatch('main')
   })
 }
@@ -20,7 +20,7 @@ const signInSpa = ({ dispatch }) => {
 }
 
 const main = async ({ commit } ) => {
-  await axios.get(process.env.VUE_APP_API_URL + '/whoami').then((response) => {
+  await axios.get(process.env.VUE_APP_API_URL + route('api.user.whoami')).then((response) => {
     commit('SET_USER', response.data)
   }).catch(() => {
     commit('SET_USER', null)
