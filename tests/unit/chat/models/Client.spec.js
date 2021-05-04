47da2d399a4f6abf78924993c20f013a7dbbdd4f
Kamil Khismatullin <kami16ru@gmail.com>
2021-05-04 14:07:10 +0300

    describe fields

--- tests/unit/chat/models/Client.spec.js
+++ tests/unit/chat/models/Client.spec.js
@@ -1,20 +1,24 @@
 import Client from '@/apps/chat/models/Client'
+import RoleCollection from '@/js/models/RoleCollection'
 
 import ModelEntity from '@/lib/ModelEntity'
 
 function createClient(options = {}) {
   // const client = new Client()
-  // const data = options.data || {
-  //   id: Date.now(),
-  //   channel_name: '7178e6f1-f72e-38cd-a6aa-fd4f84257940',
-  //   message: 'Jest test message',
-  //   operators_online: 1,
-  //   created_at: '2021-05-03T05:45:57.000000Z',
-  //   updated_at: '2021-05-03T05:45:57.000000Z',
-  //   client: client
-  // }
+  const data = options.data || {
+    id: Date.now(),
+    name: 'Ethyl Sanford',
+    email: 'tfahey@example.org',
+    created_at: '2021-05-03T05:45:57.000000Z',
+    updated_at: '2021-05-03T05:45:57.000000Z',
+    userRoles: [{
+      id: 1,
+      hasPermissions: [],
+      name: 'Client'
+    }]
+  }
 
-  const data = options.data || {}
+  // const data = options.data || {}
 
   return new Client({
     data,
@@ -25,8 +29,6 @@ function createClient(options = {}) {
 describe('Class Client', () => {
   test('new works', () => {
     expect(createClient()).toBeInstanceOf(Client)
-
-    console.log(createClient())
   })
 
   test('extends model structure', () => {
@@ -39,19 +41,22 @@ describe('Class Client', () => {
     }).toThrow('Data must be defined')
   })
 })
-//
-// describe('fields', () => {
-//   test('Chat Request structure', () => {
-//     expect(createChatRequest()).toEqual(expect.objectContaining({
-//       $fields: expect.objectContaining({
-//         id: expect.any(Number),
-//         channel_name: expect.any(String),
-//         message: expect.any(String),
-//         operators_online: expect.any(Number),
-//         updated_at: expect.any(String),
-//         created_at: expect.any(String),
-//         client: expect.any(Client)
-//       })
-//     }))
-//   })
-// })
+
+describe('fields', () => {
+  test('Client structure', () => {
+    expect(createClient()).toEqual(expect.objectContaining({
+      $fields: expect.objectContaining({
+        id: expect.any(Number),
+        name: expect.any(String),
+        email: expect.any(String),
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+        userRoles: expect.arrayContaining([expect.objectContaining({
+          id: expect.any(Number),
+          name: expect.any(String),
+          hasPermissions: expect.any(Array)
+        })])
+      })
+    }))
+  })
+})
