78697fb4c45d078dd89ff4f6904cd49b262162a2
Kamil Khismatullin <kami16ru@gmail.com>
2021-05-04 14:18:58 +0300

    describe class ClientCollection

---
+++ tests/unit/js/models/ClientCollection.spec.js
@@ -0,0 +1,44 @@
+import ClientCollection from '@/js/models/ClientCollection'
+
+import ModelCollection from '@/js/lib/ModelCollection'
+
+function createModelCollection(data = [], options = {}) {
+  return new ClientCollection({
+    ...options,
+    data
+  })
+}
+
+describe('Class ClientCollection', () => {
+  test('new works', () => {
+    expect(createModelCollection()).toBeInstanceOf(ClientCollection)
+  })
+
+  test('Extends model structure', () => {
+    expect(createModelCollection()).toBeInstanceOf(ModelCollection)
+  })
+})
+
+// describe('collection', () => {
+//   test('Call with empty data end expect empty array', () => {
+//     expect(createModelCollection()).toEqual(expect.objectContaining({
+//       $collection: expect.arrayContaining([])
+//     }))
+//   })
+// })
+//
+// describe('customizations', () => {
+//   test('primary key must be id', () => {
+//     const modelCollection = createModelCollection()
+//
+//     expect(modelCollection.$options.primaryKey).toBe('id')
+//   })
+//
+//   test('we can\'t customize the primaryKey', () => {
+//     const modelCollection = createModelCollection([],{
+//       primaryKey: 'name'
+//     })
+//
+//     expect(modelCollection.$options.primaryKey).toBe('id')
+//   })
+// })
