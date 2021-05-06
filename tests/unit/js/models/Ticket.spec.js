1996aff872ae490d259da09bfa0e146d6d7f79e5
Kamil Khismatullin <kami16ru@gmail.com>
2021-05-06 12:50:32 +0300

    Ticket.spec

--- tests/unit/js/models/Ticket.spec.js
+++ tests/unit/js/models/Ticket.spec.js
@@ -3,20 +3,33 @@ import Ticket from '@/js/models/Ticket'
 import ModelEntity from '@/js/lib/ModelEntity'
 
 function createModel(options = {}) {
-  // const data = options.data || {
-  //   id: Date.now(),
-  //   name: 'Ethyl Sanford',
-  //   email: 'tfahey@example.org',
-  //   created_at: '2021-05-03T05:45:57.000000Z',
-  //   updated_at: '2021-05-03T05:45:57.000000Z',
-  //   userRoles: [{
-  //     id: 1,
-  //     hasPermissions: [],
-  //     name: 'Client'
-  //   }]
-  // }
-
-  const data = options.data || {}
+  const data = options.data || {
+    id: Date.now(),
+    issue: 'Magnam fuga consequuntur incidunt sit sit.',
+    description: 'Voluptatem porro animi eligendi molestiae. Quo rerum totam similique ut autem nihil. Iste dicta quia placeat at maiores quisquam.',
+    created_at: '2021-05-03T05:45:57.000000Z',
+    updated_at: '2021-05-03T05:45:57.000000Z',
+    ticketChatRequests: [{
+      id: Date.now(),
+      channel_name: '7178e6f1-f72e-38cd-a6aa-fd4f84257940',
+      message: 'Jest test message',
+      operators_online: 1,
+      created_at: '2021-05-03T05:45:57.000000Z',
+      updated_at: '2021-05-03T05:45:57.000000Z',
+      client: {
+        id: Date.now(),
+        name: 'Ethyl Sanford',
+        email: 'tfahey@example.org',
+        created_at: '2021-05-03T05:45:57.000000Z',
+        updated_at: '2021-05-03T05:45:57.000000Z',
+        userRoles: [{
+          id: 1,
+          hasPermissions: [],
+          name: 'Client'
+        }]
+      }
+    }]
+  }
 
   return new Ticket({
     data,
@@ -47,15 +60,27 @@ describe('fields', () => {
         id: expect.any(Number),
         issue: expect.any(String),
         description: expect.any(String),
-        name: expect.any(String),
-        email: expect.any(String),
         created_at: expect.any(String),
         updated_at: expect.any(String),
         ticketChatRequests: expect.arrayContaining([expect.objectContaining({
           id: expect.any(Number),
+          channel_name: expect.any(String),
           message: expect.any(String),
-          name: expect.any(String),
-          hasPermissions: expect.any(Array)
+          operators_online: expect.any(Number),
+          updated_at: expect.any(String),
+          created_at: expect.any(String),
+          client: expect.objectContaining({
+            id: expect.any(Number),
+            name: expect.any(String),
+            email: expect.any(String),
+            created_at: expect.any(String),
+            updated_at: expect.any(String),
+            userRoles: expect.arrayContaining([expect.objectContaining({
+              id: expect.any(Number),
+              name: expect.any(String),
+              hasPermissions: expect.any(Array)
+            })])
+          })
         })])
       })
     }))
